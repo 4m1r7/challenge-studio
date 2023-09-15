21d2fea9ef88b20a580072cd39961c7d7db610a3
Amir <amir@Amirs-MacBook-Air.local>
2023-09-16 00:18:19 +0330

    chore(MediaModal): "media modal cursor and fullscreen"

--- src/components/MediaModal.tsx
+++ src/components/MediaModal.tsx
@@ -6,6 +6,8 @@ import DarkClose from '~/svg/close-dark.svg';
 import LightClose from '~/svg/close-light.svg';
 import DarkChevron from '~/svg/down-chev-dark.svg';
 import LightChevron from '~/svg/down-chev-light.svg';
+import DarkFullscreen from '~/svg/fullscreen-dark.svg';
+import LightFullscreen from '~/svg/fullscreen-light.svg';
 
 interface MediaModalProps {
   mediaUrls: { mediaUrl: string; mediaType: string }[];
@@ -27,6 +29,7 @@ export default function MediaModal({
   theme,
 }: MediaModalProps) {
   const [isVideoPlaying, setIsVideoPlaying] = useState(false);
+  const [isFullScreen, setIsFullScreen] = useState(false);
 
   useEffect(() => {
     // Pause video when modal is closed
@@ -50,27 +53,50 @@ export default function MediaModal({
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
-      className={`fixed inset-0 z-50 flex flex-col gap-5 p-20 pt-28
+      className={`fixed inset-0 z-50 flex flex-col gap-5
                 ${
                   theme == 'light'
                     ? 'bg-customGray text-customDarkBlue'
                     : 'bg-customDarkBlue text-customGray'
-                }`}
+                }
+                ${isFullScreen == true ? 'px-6 py-1' : 'p-20 pt-28'}`}
     >
       {/* Gallery Image and Controls */}
-      <div className=' z-50 flex w-full flex-grow items-center justify-between gap-28 '>
+      <div
+        className={` z-50 flex w-full flex-grow items-center justify-between 
+                      ${isFullScreen == true ? 'gap-0' : 'gap-24'}`}
+      >
+        {/* Close Button */}
         {theme == 'light' ? (
           <DarkClose
-            className=' absolute right-24 top-28 h-6 w-6 cursor-pointer '
+            className={` absolute top-28 h-6 w-6 cursor-pointer 
+                        ${isFullScreen == true ? 'right-8' : 'right-20'}`}
             onClick={handleClose}
           />
         ) : (
           <LightClose
-            className=' absolute right-24 top-28 h-6 w-6 cursor-pointer '
+            className={` absolute  top-28 h-6 w-6 cursor-pointer 
+                        ${isFullScreen == true ? 'right-8' : 'right-20'}`}
             onClick={handleClose}
           />
         )}
 
+        {/* Fullscreen Button */}
+        {theme == 'light' ? (
+          <DarkFullscreen
+            className={` absolute  top-28 h-8 w-8 cursor-pointer 
+                        ${isFullScreen == true ? 'left-8' : 'left-20'}`}
+            onClick={() => setIsFullScreen(!isFullScreen)}
+          />
+        ) : (
+          <LightFullscreen
+            className={` absolute  top-28 h-8 w-8 cursor-pointer 
+                        ${isFullScreen == true ? 'left-8' : 'left-20'}`}
+            onClick={() => setIsFullScreen(!isFullScreen)}
+          />
+        )}
+
+        {/* Previous Button */}
         {theme == 'light' ? (
           <DarkChevron
             className='top-1/2 h-10 w-10 -translate-y-1/2 rotate-90 transform cursor-pointer'
@@ -83,6 +109,7 @@ export default function MediaModal({
           />
         )}
 
+        {/* Media */}
         <div className=' relative flex h-full w-full flex-col'>
           {mediaUrls[currentMediaIndex].mediaType === 'image' ? (
             <>
@@ -94,11 +121,11 @@ export default function MediaModal({
                 className='object-contain'
               />
               <div
-                className=' absolute left-0 top-0 h-full w-1/2 cursor-w-resize'
+                className='prev-overlay left-chev-cursor absolute left-0 top-0 h-full w-1/2'
                 onClick={onPrev}
               />
               <div
-                className=' absolute right-0 top-0 h-full w-1/2 cursor-e-resize'
+                className='next-overlay right-cursor right-chev-cursor absolute right-0 top-0 h-full w-1/2'
                 onClick={onNext}
               />
             </>
@@ -113,6 +140,7 @@ export default function MediaModal({
           )}
         </div>
 
+        {/* Next Button */}
         {theme == 'light' ? (
           <DarkChevron
             className='top-1/2 h-10 w-10 -translate-y-1/2 -rotate-90 transform cursor-pointer'
@@ -127,42 +155,44 @@ export default function MediaModal({
       </div>
 
       {/* Gallery Thumbnails */}
-      <div className=' mt-4 flex flex-nowrap space-x-2 overflow-x-scroll'>
-        {mediaUrls.map((item, index) => (
-          <div
-            key={item.mediaUrl}
-            onClick={() => {
-              if (index !== currentMediaIndex) {
-                setCurrentMediaIndex(index);
-                setIsVideoPlaying(false);
-              }
-            }}
-            className={`relative m-1 h-20 w-20 min-w-[5rem] cursor-pointer rounded-sm ${
-              index === currentMediaIndex
-                ? 'border-blue-500'
-                : 'border-gray-300'
-            } overflow-hidden border-2`}
-          >
-            {item.mediaType === 'image' ? (
-              <Image
-                src={item.mediaUrl || ''}
-                fill
-                className='object-cover'
-                alt='Gallery Image'
-              />
-            ) : (
-              <video
-                poster={item.mediaUrl || ''}
-                controls
-                preload='metadata'
-                className='aspect-square w-full object-cover'
-              >
-                <source src={item.mediaUrl || ''} type='video/mp4' />
-              </video>
-            )}
-          </div>
-        ))}
-      </div>
+      {!isFullScreen && (
+        <div className=' mt-4 flex flex-nowrap space-x-2 overflow-x-scroll'>
+          {mediaUrls.map((item, index) => (
+            <div
+              key={item.mediaUrl}
+              onClick={() => {
+                if (index !== currentMediaIndex) {
+                  setCurrentMediaIndex(index);
+                  setIsVideoPlaying(false);
+                }
+              }}
+              className={`relative m-1 h-20 w-20 min-w-[5rem] cursor-pointer rounded-sm ${
+                index === currentMediaIndex
+                  ? 'border-blue-500'
+                  : 'border-gray-300'
+              } overflow-hidden border-2`}
+            >
+              {item.mediaType === 'image' ? (
+                <Image
+                  src={item.mediaUrl || ''}
+                  fill
+                  className='object-cover'
+                  alt='Gallery Image'
+                />
+              ) : (
+                <video
+                  poster={item.mediaUrl || ''}
+                  controls
+                  preload='metadata'
+                  className='aspect-square w-full object-cover'
+                >
+                  <source src={item.mediaUrl || ''} type='video/mp4' />
+                </video>
+              )}
+            </div>
+          ))}
+        </div>
+      )}
     </motion.div>
   );
 }
