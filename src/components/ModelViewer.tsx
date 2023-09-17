17e9d2d3e31558ae7158ecb243e4e18b790725c1
Amir <amir@Amirs-MacBook-Air.local>
2023-09-17 17:40:37 +0330

    chore(model-viewer): "loading model file from CMS"

--- src/components/ModelViewer.tsx
+++ src/components/ModelViewer.tsx
@@ -1,14 +1,14 @@
 import { OrbitControls } from '@react-three/drei';
 import { Canvas, useFrame, useLoader } from '@react-three/fiber';
 import { useRef } from 'react';
+// Import THREE from the 'three' package
 import { Mesh } from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
-function MeshComponent() {
-  const fileUrl = '/Emdadgaran.glb';
+function MeshComponent({ fileUrl }: { fileUrl: string | null }) {
   const model = useRef<Mesh | null>(null);
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const gltf = useLoader(GLTFLoader as any, fileUrl);
+  const gltf = useLoader(GLTFLoader as any, fileUrl || '');
 
   useFrame(() => {
     if (model.current) {
@@ -23,20 +23,34 @@ function MeshComponent() {
   );
 }
 
-export default function ModelViewer() {
+export default function ModelViewer({ fileUrl }: { fileUrl: string | null }) {
   return (
     <div className='flex h-[75vh] w-full items-center justify-center'>
-      <Canvas className=''>
-        <OrbitControls />
+      {fileUrl ? (
+        <Canvas
+          camera={{
+            position: [25, 12, 25], // Adjust the position of the camera to change the initial zoom level
+          }}
+        >
+          <OrbitControls enableZoom={false} zoomSpeed={5} />
 
-        <directionalLight intensity={2} position={[1, 2, 1]} />
-        <directionalLight intensity={1} position={[-1, -1, -1]} />
-        <directionalLight intensity={0.5} position={[1, -1, -1]} />
+          {/* Ambient Light */}
+          <ambientLight intensity={0.5} />
 
-        <ambientLight intensity={0.3} />
+          {/* Directional Light (Sunlight) */}
+          <directionalLight
+            castShadow
+            intensity={2.0}
+            position={[10, 20, 10]}
+            shadow-mapSize-width={1024}
+            shadow-mapSize-height={1024}
+          />
 
-        <MeshComponent />
-      </Canvas>
+          <MeshComponent fileUrl={fileUrl} />
+        </Canvas>
+      ) : (
+        <p>Project Model Is Not Available!</p>
+      )}
     </div>
   );
 }
