4273af2aebe07026e7ac481b428f612e0fd8c4a1
Amir <amir@Amirs-MacBook-Air.local>
2023-12-06 18:33:47 +0330

    chore(_documanet): added google tag manager codes to verify google analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,21 @@
 import { Head, Html, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
   return (
     <Html lang='en'>
       <Head>
+        {/* Google tag (gtag.js) */}
+        <Script src='https://www.googletagmanager.com/gtag/js?id=G-6QJVP16KQY' />
+        <Script id='google-analytics'>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-6QJVP16KQY');
+          `}
+        </Script>
         {/* Preload Source Code Pro*/}
         <link
           rel='preload'
